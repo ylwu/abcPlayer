019eea7c2f499d1d6e7e17de9f34a7757cbe3be4
Yonglin Wu <yonglinwu92@gmail.com>
2012-10-25 21:36:36 -0400

    936

--- abcPlayer/src/player/ParserTest.java
+++ abcPlayer/src/player/ParserTest.java
@@ -28,12 +28,12 @@ public class ParserTest {
     @Test
     public void basicTest3() throws IOException{
         Lexer l = new Lexer();
-        l.tokenize("piece1.abc");
+        l.tokenize("piece2.abc");
         System.out.println(l.toString());
         Parser p = new Parser(l);
         System.out.println(p.toString());
     }      
-        //@Test
+        @Test
         public void basicTestChords() throws IOException{
             Lexer l = new Lexer();
             l.tokenize("lexer_legal_chords.abc");
@@ -42,7 +42,7 @@ public class ParserTest {
             assertEquals(expected,p.toString());
         }
         
-        //@Test
+        @Test
         public void basicTestSingles() throws IOException{
             Lexer l = new Lexer();
             l.tokenize("lexer_legal_single.abc");
@@ -64,28 +64,9 @@ public class ParserTest {
             assertEquals(expected,p.toString());
         }
         
-        @Test
-        public void randomTest() throws IOException{
-            Lexer l = new Lexer();
-            l.tokenize("random.abc");
-            Parser p = new Parser(l);
-            String expected = "Voice:1---Section( Single{e3.0} Single{G1.0} Single{f1.0} Single{e1.0}" +
-            		" ) Section( Single{d3.0} Single{F1.0} Single{e1.0} Single{d1.0} ) Section" +
-            		"( Single{c3.0} Single{E1.0} Single{d1.0} Single{c1.0} ) Section( Single{B2.0} " +
-            		"Single{z1.0} Single{E1.0} Single{e1.0} Single{z1.0} ) Section( Single{z1.0} " +
-            		"Single{e1.0} Single{e'1.0} Single{z1.0} Single{z1.0} Single{^d1.0} )  " +
-            		"Voice:2---Section( Single{C,1.0} Single{E,1.0} Single{C1.0} Single{z1.0} " +
-            		"Single{z2.0} ) Section( Single{G,1.0} Single{,1.0} Single{G,1.0} Single{B,1.0} " +
-            		"Single{z1.0} Single{z2.0} ) Section( Single{A,1.0} Single{,1.0} Single{E,1.0}" +
-            		" Single{A,1.0} Single{z1.0} Single{z2.0} ) Section( Single{E,1.0} Single{,1.0} " +
-            		"Single{E,1.0} Single{E1.0} Single{z1.0} Single{z1.0} Single{E1.0} ) Section( " +
-            		"Single{e1.0} Single{z1.0} Single{z1.0} Single{^d1.0} Single{e1.0} Single{z1.0} )  ";
-            assertEquals(expected,p.toString());
-            System.out.println(p.toString());
-        }
         
         @Test
-        public void spaceTest() throws IOException{
+        public void BasicTestSpaces() throws IOException{
             Lexer l = new Lexer();
             l.tokenize("parser_space.abc");
             Parser p = new Parser(l);
